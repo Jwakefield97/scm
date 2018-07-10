ac2231cab85142348dab2dfd9e760bae9634ed1c
jwakefield97 <jacobstevenwakefield@gmail.com>
2018-07-09 21:42:30 -0500

    add modification times of files

--- lib/utils/fileUtil.go
+++ lib/utils/fileUtil.go
@@ -10,13 +10,16 @@ import (
 //dir is directory to scan
 func ScanForFiles(dir string) map[string]interface{} {
 	returnObj := map[string]interface{}{}
-	files := []string{}
+	files := []map[string]string{}
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() {
+			filePath := map[string]string{}
+			filePath["path"] = path
+			filePath["modTime"] = info.ModTime().String() //save modification time as string
 			if len(path) < 4 {
-				files = append(files, path)
+				files = append(files, filePath)
 			} else if path[:3] != ".scm" { //file not in .scm directory
-				files = append(files, path)
+				files = append(files, filePath)
 			}
 		}
 		return nil
